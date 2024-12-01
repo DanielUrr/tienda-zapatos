fccc24c51c922c9525091237985b59af7a222256
DanielUrr <cesarandresantolinez@gmail.com>
2024-11-30 22:50:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ document.getElementById("contactForm").addEventListener("submit", function (e) {
     alert("¡Gracias por contactarnos! Te responderemos pronto.");
     document.getElementById("contactForm").reset();
   });
-  
+  const API_URL = "https://678bbbaf-562e-4a71-8fe7-f21a652d3d91-00-yi0lkmdntppj.spock.replit.dev/";
